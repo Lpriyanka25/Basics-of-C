3e2d58e7fa7ab344543b655e8be301f4135975a8
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-10-08 19:41:50 +0530

    Create ascending_order.c

---
+++ ascending_order.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+	int a[100],n,i,j,t;
+	printf("\n Array size:");
+	scanf("%d",&n);
+	printf("Enter the elements of array:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(a[j]>a[i])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	printf("\n\nAscending order is:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d",a[i]);
+	}
+}
