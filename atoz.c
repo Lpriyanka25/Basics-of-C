c90d27240e6b9e7d6305b34f3f5844fad4b022a7
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-10-12 00:00:06 +0530

    Create atoz.c

---
+++ atoz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Characters from A to Z:\n");
+
+    // Loop to display characters from 'A' to 'Z'
+    for (ch = 'A'; ch <= 'Z'; ch++) {
+        printf("%c ", ch);
+    }
+
+    printf("\n");
+
+    return 0;
+}
