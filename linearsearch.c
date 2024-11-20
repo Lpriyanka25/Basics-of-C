884592e0c7b082016c85c654e4598841ca2b1195
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2024-11-20 23:16:45 +0530

    Create linearsearch.c

---
+++ linearsearch.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; // Return index if target is found
+        }
+    }
+    return -1; // Return -1 if target is not found
+}
